9d4e033c407f6dd00bf67f08430fa2764d53203f
joymohanta <joymohanta06@gmail.com>
2022-05-07 00:55:01 +0600

    post add new Item done

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -3,7 +3,21 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+    const url = `http://localhost:5000/item`;
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
   return (
     <div className="text-center my-4">
       <h2>This is Add item page</h2>
