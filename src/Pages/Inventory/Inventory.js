190b46e40d7f11f1c4e1b3b2dac1ba8f39bee043
joymohanta <joymohanta06@gmail.com>
2022-05-06 20:07:10 +0600

    added quantity button

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import "./Inventory.css";
 
 const Inventory = () => {
   const { id } = useParams();
@@ -12,9 +13,71 @@ const Inventory = () => {
       .then((data) => setItem(data));
   }, []);
 
+  function getCurrentQuantity() {
+    const quantity = document.getElementById("total-quantity");
+    const beforeQuantity = parseInt(quantity.innerText);
+    return beforeQuantity;
+  }
+  function totalQuantity(amount, isAdd) {
+    const quantity = document.getElementById("total-quantity");
+    const beforeQuantity = getCurrentQuantity();
+    if (isAdd === true) {
+      quantity.innerText = beforeQuantity + amount;
+    } else {
+      quantity.innerText = beforeQuantity - amount;
+    }
+  }
+  const handleQuantity = () => {
+    const amountField = document.getElementById("quantity-input");
+    const amount = parseInt(amountField.value);
+    amountField.value = "";
+    if (amount > 0) {
+      totalQuantity(amount, true);
+    }
+  };
+
+  const handleDeliver = () => {
+    const quantity = document.getElementById("total-quantity");
+    const beforeQuantity = quantity.innerText;
+    quantity.innerText = parseInt(beforeQuantity) - 1;
+  };
+
   return (
-    <div>
-      <h2>Your selected Item detail id is: {item.name} </h2>
+    <div className="selected_item_detail">
+      <h2>Your selected Item detail id is: {item._id} </h2>
+      <img src={item.img} alt="" />
+      <h3>Product name is : {item.name}</h3>
+      <h4>Product supplier : {item.supplier}</h4>
+      <h4>Price : {item.price}</h4>
+      <h4>
+        Total quantity : <span id="total-quantity"> {item.quantity} </span>
+      </h4>
+      <p> {item.description} </p>
+      <button
+        id="deliver-button"
+        onClick={handleDeliver}
+        className="btn btn-primary mb-3"
+      >
+        Deliver One Item
+      </button>
+      <div>
+        <h5>Enter your new quantity item here</h5>
+        <input
+          id="quantity-input"
+          type="text"
+          placeholder="Quantity number"
+          required
+        />
+        <br />
+        <br />
+        <button
+          id="quantity-button"
+          onClick={handleQuantity}
+          className="btn btn-success"
+        >
+          Add Quantity
+        </button>
+      </div>
     </div>
   );
 };
